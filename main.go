f2886ac168dec0f4334d15fdeecd946f18ca6bb9
lurey2 <reymerretamozolopez@gmail.com>
2023-12-30 20:18:54 -0500

    test

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+var ginLambda *ginadapter.GinLambda
+var pathOrigin = "/api/"
+
+func main() {
+	r := setupRouter()
+	ginLambda = ginadapter.New(r)
+
+	lambda.Start(Handler)
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	SetRouter(r)
+	return r
+}
+
+func SetRouter(r *gin.Engine) {
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "DELETE", "POST", "GET"},
+		AllowHeaders:     []string{"Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "*"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Funciona correctamente")
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+}
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return ginLambda.ProxyWithContext(ctx, request)
+}
