9425a0dccf7a5e065a33bc25b36f48cc17a7b484
lurey2 <reymerretamozolopez@gmail.com>
2023-12-30 21:04:45 -0500

    uodate

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"context"
 	"net/http"
-	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -30,17 +28,6 @@ func setupRouter() *gin.Engine {
 }
 
 func SetRouter(r *gin.Engine) {
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"PUT", "PATCH", "DELETE", "POST", "GET"},
-		AllowHeaders:     []string{"Authorization", "Content-Type"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "*"
-		},
-		MaxAge: 12 * time.Hour,
-	}))
 
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Funciona correctamente")
