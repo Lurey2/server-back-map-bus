7589aa48943c253fc29a4f4b61d7109122f47281
lurey2 <reymerretamozolopez@gmail.com>
2024-01-01 20:39:08 -0500

    update

--- controllers/userController.go
+++ controllers/userController.go
@@ -105,6 +105,7 @@ func Login(c *gin.Context) {
 		return
 	}
 
+	c.SetSameSite(http.SameSiteNoneMode)
 	c.SetCookie("SESSIONID", token, 15552000, "/", "http://ec2-54-196-14-202.compute-1.amazonaws.com", false, true)
 	c.JSON(http.StatusOK, conf.Response{
 		Status: conf.Succes,
@@ -191,7 +192,7 @@ func LoginGoogleInSignUp(c *gin.Context) {
 		})
 		return
 	}
-
+	c.SetSameSite(http.SameSiteNoneMode)
 	c.SetCookie("SESSIONID", tokenEncript, 15552000, "/", "http://ec2-54-196-14-202.compute-1.amazonaws.com", true, true)
 
 	c.JSON(http.StatusOK, conf.Response{
@@ -214,6 +215,8 @@ func LogoutUser(c *gin.Context) {
 			Status: conf.Error,
 		})
 	}
+
+	c.SetSameSite(http.SameSiteNoneMode)
 	c.SetCookie("SESSIONID", "", -1, "/", "http://ec2-54-196-14-202.compute-1.amazonaws.com", true, true)
 
 	c.JSON(http.StatusOK, conf.Response{
